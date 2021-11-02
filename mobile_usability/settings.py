5533c49ee47801277bddd8ba4d55945b3f779872
Claudio Raverta <claudioraverta@hotmail.com>
2021-11-02 17:58:54 -0300

    wss

--- mobile_usability/settings.py
+++ mobile_usability/settings.py
@@ -139,3 +139,34 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+# Django Logging Information
+LOGGING = {
+    # Define the logging version
+    'version': 1,
+    # Enable the existing loggers
+    'disable_existing_loggers': False,
+
+    # Define the handlers
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'djangoapp.log',
+        },
+
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+
+   # Define the loggers
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+
+        },
+    },
+}
